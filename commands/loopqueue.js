45e170b337e8d45ef2df26c192b3dba2de562893
Force Entries <84038179+ForceEntries@users.noreply.github.com>
2021-08-25 16:29:48 +0700

    Update loopqueue.js

--- commands/loopqueue.js
+++ commands/loopqueue.js
@@ -3,7 +3,7 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
     name: "loopqueue",
-    description: "Loop the whole queue",
+    description: "Ulangi semua daftar antrian",
     usage: "",
     permissions: {
       channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
