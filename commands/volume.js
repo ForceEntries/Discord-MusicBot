26ad786b3c7a1a06a3105601479fd98d5d40d78a
Force Entries <84038179+ForceEntries@users.noreply.github.com>
2021-08-25 16:42:24 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -3,7 +3,7 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
     name: "volume",
-    description: "Check or change the current volume",
+    description: "Cek atau ubah volume saat ini",
     usage: "<volume>",
     permissions: {
         channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
