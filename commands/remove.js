0640cd7b3965bdfc08769a62b3ebf7726e016ebb
Force Entries <84038179+ForceEntries@users.noreply.github.com>
2021-08-25 16:35:25 +0700

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -3,7 +3,7 @@ const { TrackUtils } = require("erela.js");
 
   module.exports = {
     name: "remove",
-    description: `Remove a song from the queue`,
+    description: `hapus semua daftar antrian`,
     usage: "[number]",
     permissions: {
       channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
