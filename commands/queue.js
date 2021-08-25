107193f9f0c71c94f9c91f94bf4b6ec7c0a4517d
Force Entries <84038179+ForceEntries@users.noreply.github.com>
2021-08-25 16:34:29 +0700

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -4,7 +4,7 @@ const prettyMilliseconds = require("pretty-ms");
 
 module.exports = {
   name: "queue",
-  description: "Shows all currently enqueued songs",
+  description: "Menampilkan semua daftar antrian",
   usage: "",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
